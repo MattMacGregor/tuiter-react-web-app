de7913184c3de697622504e197e753678540529d
Matt MacGregor <matthew.macgregor.g@gmail.com>
2022-10-15 20:41:33 -0400

    Lab 5 done

---
+++ public/labs/a5/todos/TodoItem.js
@@ -0,0 +1,11 @@
+const TodoItem = (todo) => {
+   return(`
+       <li>
+          <input type="checkbox"
+            ${todo.done ? 'checked' : ''}/>
+          ${todo.title}
+
+       </li>
+   `);
+}
+export default TodoItem;
