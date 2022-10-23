93ad61e3b8bb9b52c7e8a246f036471a7889fe42
Matt MacGregor <matthew.macgregor.g@gmail.com>
2022-10-23 19:52:38 -0400

    Fix href warnings

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -7,37 +7,37 @@ const NavigationSidebar = (
 ) => {
   return(
    <div className="list-group">
-     <a className="list-group-item">Tuiter</a>
+     <a className="list-group-item" href="">Tuiter</a>
      <a className={`list-group-item
-                    ${active === 'home'?'active':''}`}>
+                    ${active === 'home'?'active':''}`} href="./home.html">
        Home
      </a>
      <a className={`list-group-item
-                    ${active === 'explore'?'active':''}`}>
+                    ${active === 'explore'?'active':''}`} href="./explore.html">
        Explore
      </a>
      <a className={`list-group-item
-                    ${active === 'notifications'?'active':''}`}>
+                    ${active === 'notifications'?'active':''}`} href="./notifications.html">
        Notifications
      </a>
      <a className={`list-group-item
-                    ${active === 'messages'?'active':''}`}>
+                    ${active === 'messages'?'active':''}`} href="./messages.html">
        Messages
      </a>
      <a className={`list-group-item
-                    ${active === 'bookmarks'?'active':''}`}>
+                    ${active === 'bookmarks'?'active':''}`} href="./bookmarks.html">
        Bookmarks
      </a>
      <a className={`list-group-item
-                    ${active === 'lists'?'active':''}`}>
+                    ${active === 'lists'?'active':''}`} href="./list.html">
        Lists
      </a>
      <a className={`list-group-item
-                    ${active === 'profile'?'active':''}`}>
+                    ${active === 'profile'?'active':''}`} href="./profile.html">
        Profile
      </a>
      <a className={`list-group-item
-                    ${active === 'more'?'active':''}`}>
+                    ${active === 'more'?'active':''}`} href="./more.html">
        More
      </a>
    </div>
